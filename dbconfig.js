f24e17a57ace03f1bea42f6d7e5c6c6e887360d6
imrajat24 <imrajat@gmail.com>
2023-10-01 03:30:37 +0530

    added some db changes

---
+++ dbconfig.js
@@ -0,0 +1,14 @@
+const config = {
+  user: "DESKTOP-9SI1KF9gulat",
+  password: "2412",
+  server: "DESKTOP-9SI1KF9SQLEXPRESS",
+  database: "testDb",
+  options: {
+    trustedconnection: true,
+    enableArithAbort: true,
+    instancename: "SQLEXPRESS",
+  },
+  port: 5050,
+};
+
+module.exports = config;
